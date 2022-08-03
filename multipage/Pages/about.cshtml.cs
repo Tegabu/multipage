788d1331ef633517fef1908d987af863e2f56286
Tegabu <yohanneswekesa3@gmail.com>
2022-08-03 11:15:42 +0100

    Add project files.

---
+++ multipage/Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace multipage.Pages
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
